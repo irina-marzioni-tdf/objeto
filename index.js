f5b1d4ab62cef4e474becc0512791aa6d5f7daa2
nadia <tnadiatolaba@tdf.edu.ar>
2022-09-12 20:01:41 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let readlineSync = require('readline-sync');
+let vueltas = Number (readlineSync.question('Ingrese cantidad de vueltas: ')); // se agrega solicitud de cantidad de vueltas
+let tiempoVuelta=[];
+let tiempoTotal = 0;
+cargarTiempoVueltas(vueltas); // se agrega las vueltas como parametro para cada funcion
+calcularTiempoTotal(vueltas);
+calcularPromedioVuelta(vueltas);
+
+function cargarTiempoVueltas(vueltas) {    //funcion para cargar el tiempo de cada vuelta
+    for(let i=0 ;i < vueltas ; i++) {
+    tiempoVuelta[i]= readlineSync.questionInt('Ingrese tiempo en minutos de la vuelta '+ (i+1) + ' :');
+    console.log('El tiempo de la vuelta '+ (i+1) + ' es:' + tiempoVuelta[i]+' minutos'); 
+    }
+}
+function calcularTiempoTotal(vueltas) { // funcion para calcular el tiempo total de vueltas
+    for(let i=0 ;i < vueltas ; i++) {
+    tiempoTotal += tiempoVuelta[i];
+    } 
+console.log('El tiempo total es: '+ tiempoTotal+' minutos');
+}
+function calcularPromedioVuelta(vueltas) { 
+    let promedio = tiempoTotal / vueltas;
+    console.log('El tiempo promedio de vueltas es: '+promedio+' minutos');
+}
